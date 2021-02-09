117274181874dd4fc2a19c884bcdf5f124ebf8a3
Rusen Minchev <rusenminchev@gmail.com>
2021-02-10 00:03:25 +0200

    add

---
+++ 01. Inheritance - Lab/05. Stack of Strings/StackOfStrings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            return this.Count == 0;
+        }
+
+        public Stack<string> AddRange(IEnumerable<string> values)
+        {
+            foreach (var value in values)
+            {
+                this.Push(value);
+            }
+
+            return this;
+        }
+    }
+}
---
+++ 01. Inheritance - Lab/05. Stack of Strings/StartUp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>()
+            {
+                "stringOne",
+                "stringTwo",
+                "stringThree"     
+            };
+
+            StackOfStrings stack = new StackOfStrings();
+
+            Console.WriteLine(stack.IsEmpty());
+
+            stack.Push("first element");
+
+            stack.AddRange(list);
+
+            Console.WriteLine(stack.IsEmpty());
+
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
