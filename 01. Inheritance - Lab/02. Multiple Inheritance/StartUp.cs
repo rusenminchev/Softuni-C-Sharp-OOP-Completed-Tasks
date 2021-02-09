bdbbfe552f12e69388d7197994ba9aaa11735fcb
Rusen Minchev <rusenminchev@gmail.com>
2021-02-10 00:02:52 +0200

    add

---
+++ 01. Inheritance - Lab/02. Multiple Inheritance/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Farm
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Puppy puppy = new Puppy();
+            puppy.Weep();
+            puppy.Weep();
+        }
+    }
+}
