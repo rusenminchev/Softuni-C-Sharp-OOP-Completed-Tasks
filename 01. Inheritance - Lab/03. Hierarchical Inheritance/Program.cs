7194611b495a8f77d61f369109b0ea19335d7157
Rusen Minchev <rusenminchev@gmail.com>
2021-02-10 00:02:58 +0200

    add

---
+++ 01. Inheritance - Lab/03. Hierarchical Inheritance/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03._Hierarchical_Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
