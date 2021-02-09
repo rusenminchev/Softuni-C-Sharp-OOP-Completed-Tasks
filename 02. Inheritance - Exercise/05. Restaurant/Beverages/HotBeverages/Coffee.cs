97c32d639788c5c7f247d21dd48449a13162def4
Rusen Minchev <rusenminchev@gmail.com>
2021-02-10 00:04:31 +0200

    add

---
+++ 02. Inheritance - Exercise/05. Restaurant/Beverages/HotBeverages/Coffee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Transactions;
+
+namespace Restaurant.Beverages.HotBeverages
+{
+    public class Coffee : HotBeverage
+    {
+        //Назин за сетване на defaulf-ни стойности при наследяване
+        private const double CoffeeMilliliters = 50;
+        private const decimal CoffeePrice = 3.50m;
+
+        public Coffee(string name, double caffeine)
+        : base(name, CoffeePrice, CoffeeMilliliters)
+        {
+            this.Caffeine = caffeine;
+        }
+
+        double Caffeine { get; }
+
+    }
+}
---
+++ 02. Inheritance - Exercise/05. Restaurant/Foods/Starter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant.Foods
+{
+    public class Starter : Food
+    {
+        public Starter(string name, decimal price, double grams)
+           : base(name, price, grams)
+        {
+
+        }
+    }
+}
