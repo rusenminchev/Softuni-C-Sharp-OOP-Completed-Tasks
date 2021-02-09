9b0a2709f2fa860bafd8f39dd25d5fdbea149b9f
Rusen Minchev <rusenminchev@gmail.com>
2021-02-10 00:03:04 +0200

    add

---
+++ 01. Inheritance - Lab/04. Random List/RandomList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        public string RandomString()
+        {
+            Random random = new Random();
+            int randomIndex = random.Next(0, this.Count);
+            string randomElement = this[randomIndex];
+            this.RemoveAt(randomIndex);
+
+            return randomElement;
+        }
+    }
+}
---
+++ 01. Inheritance - Lab/04. Random List/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            RandomList list = new RandomList();
+            list.Add("Ivan");
+            list.Add("Ceco");
+            list.Add("Ivo");
+            list.Add("Gosho");
+
+            Console.WriteLine(list.RandomString()); ;
+
+            Console.WriteLine();
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
