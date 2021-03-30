cebecb1f7821e87150ea92fe5d4530d58d621a7b
Rusen Minchev <rusenminchev@gmail.com>
2021-03-31 00:04:37 +0300

    add

---
+++ 12. Reflection and Attributes - Exercise/01. Command Pattern/Core/CommandInterpreter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+using CommandPattern.Core.Contracts;
+
+namespace CommandPattern.Core
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        public string Read(string args)
+        {
+            string[] argsTokens = args
+                .Split(' ', System.StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            string commandName = argsTokens[0];
+
+            string[] commandArgs = argsTokens
+                .Skip(1)
+                .ToArray();
+
+            //Get the assembly in order to get the types
+            Assembly assembly = Assembly.GetCallingAssembly();
+
+            //Get all types in the current assembly in order to create instance of the command and call it`s execute() method.
+            
+            Type commandType = assembly
+                .GetTypes()
+                .FirstOrDefault(t => t.Name.ToLower().StartsWith(commandName.ToLower()));
+
+            if (commandType == null)
+            {
+                throw new ArgumentException("Invalid command type");
+            }
+
+            //If the object that we want to create instance of has a constructor we have to give the parameters of this constructor
+            //We can do this by creating an array with the parameters like this
+            //object[] ctorArgs = new object[2] {"Name" , 17};
+            //After that we have to add it when we create the concrete instance.
+            //ICommand commandInstance = (ICommand)Activator.CreateInstance(commandType, ctorArgs);
+
+            ICommand commandInstance = (ICommand)Activator.CreateInstance(commandType);
+
+            string result = commandInstance.Execute(commandArgs);
+
+            return result;
+        }
+    }
+}
---
+++ 12. Reflection and Attributes - Exercise/01. Command Pattern/Core/Engine.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using CommandPattern.Core.Contracts;
+
+namespace CommandPattern.Core
+{
+    public class Engine : IEngine
+    {
+        private readonly ICommandInterpreter commandInterpreter;
+
+        public Engine(ICommandInterpreter commandInterpreter)
+        {
+            this.commandInterpreter = commandInterpreter;
+        }
+
+        public void Run()
+        {
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+                try
+                {
+                    string result = commandInterpreter.Read(command);
+                    Console.WriteLine(result);
+                }
+                catch (ArgumentException ae)
+                {
+                    Console.WriteLine(ae.Message);
+                }
+            }
+        }
+    }
+}
