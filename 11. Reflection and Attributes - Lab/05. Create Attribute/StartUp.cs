c7e609ef60ddc2f3ac40530562d0a885bc8345dc
Rusen Minchev <rusenminchev@gmail.com>
2021-03-31 00:04:13 +0300

    add

---
+++ 11. Reflection and Attributes - Lab/05. Create Attribute/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AuthorProblem
+{
+    [Author("John")]
+    class StartUp
+    {
+        [Author("Inukentiy")]
+        static void Main(string[] args)
+        {
+        }
+    }
+}
