b81496897a8eb910a016ddbc0cb027bba9e5f6ce
Rusen Minchev <rusenminchev@gmail.com>
2021-03-31 00:02:55 +0300

    add

---
+++ 09. SOLID - Lab/03. Detail Printer/Manager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03.DetailPrinter
+{
+    public class Manager : Employee
+    {
+        private ICollection<string> documents;
+        public Manager(string name) : base(name)
+        {
+            this.Documents = new List<string>(documents);
+        }
+
+        public IReadOnlyCollection<string> Documents { get; private set; }
+    }
+}
